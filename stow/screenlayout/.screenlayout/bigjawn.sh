fe40718ffa29fd139c85f680506bd62c2668b3b8
Tbetcha <Troy.Boettger@gmail.com>
2023-10-11 20:37:53 -0400

    btw

---
+++ stow/screenlayout/.screenlayout/bigjawn.sh
@@ -0,0 +1,3 @@
+
+#!/bin/sh
+xrandr --output eDP-1 --mode 2560x1600 --pos 0x0 --rotate normal --output DP-1 --primary --mode 3440x1440 --pos 2560x0 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --off
---
+++ stow/screenlayout/.screenlayout/laptop.sh
@@ -0,0 +1,2 @@
+
+xrandr --output eDP-1 --mode 2560x1600 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off
