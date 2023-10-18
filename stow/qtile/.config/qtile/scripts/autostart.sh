692fd0dabe665b2a2be3d616a89fd66e4f84f112
Tbetcha <Troy.Boettger@gmail.com>
2023-10-17 20:37:13 -0400

    fix autostart and kitty

--- stow/qtile/.config/qtile/scripts/autostart.sh
+++ stow/qtile/.config/qtile/scripts/autostart.sh
@@ -9,20 +9,6 @@ function run {
   fi
 }
 
-#Set your native resolution IF it does not exist in xrandr
-#More info in the script
-#run $HOME/.config/qtile/scripts/set-screen-resolution-in-virtualbox.sh
-
-#Find out your monitor name with xrandr or arandr (save and you get this line)
-#xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
-#xrandr --output DP2 --primary --mode 1920x1080 --rate 60.00 --output LVDS1 --off &
-#xrandr --output LVDS1 --mode 1366x768 --output DP3 --mode 1920x1080 --right-of LVDS1
-#xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
-#autorandr horizontal
-
-
-# keybLayout=$(setxkbmap -v | awk -F "+" '/symbols/ {print $2}')
-
 lxsession &
 picom --config $HOME/.config/qtile/scripts/picom.conf &
 killall conky 
@@ -32,23 +18,12 @@ nm-applet &
 
 
 
-# Some ways to set your wallpaper besides variety or nitrogen
-# xargs xwallpaper --stretch < ~/.cache/wall &
-# feh --bg-fill /usr/share/backgrounds/archlinux/arch-wallpaper.jpg &
-# feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
-feh --bg-fill /usr/share/backgrounds/arcolinux/dlanham-threadtroll-3200x2000.jpg &
-# #wallpaper for other Arch based systems
-#feh --bg-fill /usr/share/archlinux-tweak-tool/data/wallpaper/wallpaper.png &
-#start the conky to learn the shortcuts
-# (conky -c $HOME/.config/qtile/scripts/system-overview) &
-
-#start sxhkd to replace Qtile native key-bindings
+feh --bg-fill ~/Pictures/colorskull.jpg &
 run sxhkd -c ~/.config/qtile/sxhkd/sxhkdrc &
 
 
 #starting utility applications at boot time
 run variety &
-# run nm-applet &
 run pamac-tray &
 run xfce4-power-manager &
 numlockx on &
@@ -58,6 +33,3 @@ blueberry-tray &
 
 #starting user applications at boot time
 run volumeicon &
-#nitrogen --restore &
-#run caffeine -a &
-#run insync start &
