40c12c8512c18d4466c3b960ac86b4f4ca1e49ff
Tbetcha <Troy.Boettger@gmail.com>
2024-07-29 22:07:18 -0400

    telescope previewer

--- stow/nvim/.config/nvim/after/plugin/telescope.lua
+++ stow/nvim/.config/nvim/after/plugin/telescope.lua
@@ -102,13 +102,13 @@ telescope.setup({
       prompt_position = "bottom",
       horizontal = {
         width_padding = 0.04,
-        height_padding = 0.1,
+        height_padding = 0,
         preview_width = 0.6,
         winblend = 10,
       },
       vertical = {
         width_padding = 0.05,
-        height_padding = 1,
+        height_padding = 0,
         preview_height = 0.5,
       },
     },
